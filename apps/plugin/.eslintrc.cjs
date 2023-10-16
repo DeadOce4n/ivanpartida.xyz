6cf1d8f3942fefa2803493298d008bae8441bbfb
Ivan Partida <ivan.97galarz@gmail.com>
2023-10-15 22:12:02 -0700

    build(plugin): fix TS config

--- apps/plugin/.eslintrc.cjs
+++ apps/plugin/.eslintrc.cjs
@@ -2,4 +2,12 @@
 module.exports = {
   root: true,
   extends: ['custom'],
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  rules: {
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/consistent-type-exports': 'error',
+  },
 };
