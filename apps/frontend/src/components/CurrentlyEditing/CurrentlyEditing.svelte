5367bc7aa88aab986e6b3061adb34d7c25b1a52a
Ivan Partida <ivan.97galarz@gmail.com>
2023-12-01 19:05:48 -0800

    feat: revamp CurrentlyEditing component

--- apps/frontend/src/components/CurrentlyEditing/CurrentlyEditing.svelte
+++ apps/frontend/src/components/CurrentlyEditing/CurrentlyEditing.svelte
@@ -2,7 +2,6 @@
   import { onDestroy, onMount } from 'svelte';
   import { decode } from '@msgpack/msgpack';
   import Git from 'icons:svelte/simple-icons/git';
-  import Neovim from 'icons:svelte/simple-icons/neovim';
   import Radar from 'icons:svelte/mingcute/radar-fill';
   import FolderFill from 'icons:svelte/mingcute/folder-2-fill';
   import Sleep from 'icons:svelte/mingcute/sleep-fill';
@@ -58,42 +57,41 @@
     icons[file?.filetype ?? 'default']?.colorClass ?? icons.default.colorClass;
 </script>
 
-<div
-  class="bg-gray flex w-full flex-col gap-4 rounded-xl p-4 shadow-md md:p-6"
-  id="currently-editing"
->
-  <div class="flex flex-row items-center gap-2">
-    <Neovim class="text-green text-lg" />
-    <span class="text-sm font-bold">Currently editing:</span>
-    <span class="ml-auto flex flex-row items-center justify-center gap-1">
-      {#if file}
-        <span class="text-xs">Live</span>
-        <Radar class="text-green" />
-      {:else}
-        <span class="text-xs">Offline</span>
-        <Sleep class="text-blue ml-auto" />
-      {/if}
-    </span>
-  </div>
-  {#if file}
-    <div class="flex flex-col gap-2 px-4">
-      <div class="flex flex-row items-center gap-2">
-        {#if file.isRepo}
-          <Git class="text-red" />
+<div class="bg-gray rounded-xl p-6 shadow-md">
+  <div class="inline-flex w-full flex-row justify-between">
+    <h3 class="!mt-0 inline-flex w-full flex-row justify-between">
+      Currently editing
+      <span class="flex flex-row items-center gap-2 self-end text-white">
+        {#if file}
+          <span class="text-xs">Live</span>
+          <Radar class="text-green" />
         {:else}
-          <FolderFill />
+          <span class="text-xs">Offline</span>
+          <Sleep class="text-blue ml-auto" />
         {/if}
-        <svelte:element
-          this={file.remoteUrl ? 'a' : 'span'}
-          href={file.remoteUrl}
-          target={file.remoteUrl ? '_blank' : null}
-        >
-          {file.baseDir}
-        </svelte:element>
-      </div>
-      <div
-        class="
-          before:from-gray-light
+      </span>
+    </h3>
+  </div>
+  <div class="flex w-full flex-col gap-4 rounded-xl" id="currently-editing">
+    {#if file}
+      <div class="flex flex-col gap-2">
+        <div class="flex flex-row items-center gap-2">
+          {#if file.isRepo}
+            <Git class="text-red" />
+          {:else}
+            <FolderFill />
+          {/if}
+          <svelte:element
+            this={file.remoteUrl ? 'a' : 'span'}
+            href={file.remoteUrl}
+            target={file.remoteUrl ? '_blank' : null}
+          >
+            {file.baseDir}
+          </svelte:element>
+        </div>
+        <div
+          class="
+          before:from-gray
           relative
           flex
           w-full
@@ -110,20 +108,20 @@
           before:to-transparent
           before:content-['']
           "
-      >
-        <svelte:component this={icon} class={iconColorClass} />
-        <div
-          class="scrollbar-hide w-full overflow-x-scroll hover:overflow-x-scroll md:overflow-x-hidden"
         >
-          <span class="pr-8">{file.filename}</span>
+          <svelte:component this={icon} class={iconColorClass} />
+          <div
+            class="scrollbar-hide w-full overflow-x-scroll hover:overflow-x-scroll md:overflow-x-hidden"
+          >
+            <span class="pr-8">{file.filename}</span>
+          </div>
         </div>
       </div>
-    </div>
-  {:else}
-    <div class="px-4">
-      I'm not editing anything right now, come back later to see what I'm up to <strong
-        >in real time</strong
-      >!
-    </div>
-  {/if}
+    {:else}
+      <div>
+        I'm not editing anything right now, come back later to see what I'm up
+        to <strong>in real time</strong>!
+      </div>
+    {/if}
+  </div>
 </div>
